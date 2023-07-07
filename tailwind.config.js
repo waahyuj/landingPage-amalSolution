6247dbcbb346e227a7096f87c76cd3580b8e53d1
Your Name <wfarghani@gmail.com>
2023-07-07 10:51:59 +0700

    fix color variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
       fontFamily: {
         poppins: ['"Poppins"', "sans-serif"],
       },
+      colors: {
+        firstPurple: '#7839F3',
+        secondPurple: '#ECE3FE',
+        thirdPurple: '#4D1CAB',
+        fourthPurple: '#211B3D',
+        green: '#31B380',
+        grey: '#5B5575',
+        colorBorder: '#ECE3FE',
+        colorFooter: '#F7F7FB',
+      },
     },
   },
   plugins: [require("tw-elements/dist/plugin.cjs")],
