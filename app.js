71897ca236f5461a87744953f54d30cc4873ea26
Your Name <wfarghani@gmail.com>
2023-07-18 11:14:38 +0700

    add signup function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const neo4j = require('neo4j-driver');
-
+const bcrypt = require('bcrypt');
 const app = express();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -28,6 +28,43 @@ app.use(express.static('node_modules'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post('/signup', function(req, res) {
+  const { username, email, password } = req.body;
+
+  const session = driver.session();
+
+  // Hash the password
+  bcrypt.hash(password, 10, function(err, hashedPassword) {
+    if (err) {
+      console.error('Error hashing password:', err);
+      res.redirect('/signup/error');
+      return;
+    }
+
+    // Create a new user node in the Neo4j database
+    session
+      .run(
+        'CREATE (u:User {username: $username, email: $email, password: $password, createdAt: TIMESTAMP(), createdBy: $username}) WITH u MATCH (l:Level), (u:User) WHERE l.label = "user" AND u.username = $username CREATE (l)-[:HAS_USER]->(u) RETURN u ',
+        { username, email, password: hashedPassword }
+      )
+      .then(result => {
+        // Close the session
+        session.close();
+
+        // Redirect to the success page or any other desired route
+        res.redirect('/signup/success');
+      })
+      .catch(error => {
+        console.error('Error creating user:', error);
+
+        // Close the session
+        session.close();
+
+        // Handle the error and redirect to an error page or show an error message
+        res.redirect('/signup/error');
+      });
+  });
+});
 
 app.get('/getall', function(req, res, next) {
   var session = driver.session();
