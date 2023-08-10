a7fcd0dd8f4168781a36810955dc81843dd553bd
Your Name <wfarghani@gmail.com>
2023-08-10 15:50:54 +0700

    improve table page tree

--- routes/common.js
+++ routes/common.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver(
+  'neo4j://localhost:7687',
+  neo4j.auth.basic('neo4j', '12345678'),
+  { disableLosslessIntegers: true }
+);
+
 /* direct untuk content header */
 router.get('/contentHeader', function(req, res, next) {
   const common = res.locals.common
@@ -21,11 +28,35 @@ router.get('/support', function(req, res, next) {
 
 /* direct untuk content tree */
 router.get('/tree', function(req, res, next) {
-  const common = res.locals.common
-   // alamat folder content tree
-  res.render('menu/tree/index', {
-    data: common
-  });
+  const session = driver.session();
+  const resultdata = []
+  session
+      .run(
+        'MATCH (p:Player) RETURN p.name AS name, p.salary AS salary, p.contract AS contract, p.status AS status',
+      )
+      .then(result => {
+        result.records.map(record => {
+          resultdata.push({
+            name: record.get('name'),
+            salary: record.get('salary'),
+            contract: record.get('contract'),
+            status: record.get('status')
+          });
+        });
+        session.close();
+        const common = res.locals.common
+        // alamat folder content tree
+        res.render('menu/tree/index', {
+          data: common, 
+          resultdata: resultdata
+        });
+      })
+      .catch(error => {
+        console.error('Error creating user:', error);
+        session.close();
+        res.redirect('/');
+      });
+  
 });
 
 module.exports = router;
